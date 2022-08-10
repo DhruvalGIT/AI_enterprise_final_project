60ad38db96463cc39037dca5fd5688a1d9667b58
DhruvalGIT <105757232+DhruvalGIT@users.noreply.github.com>
2022-08-10 15:51:52 -0400

    Add files via upload

---
+++ frontend/atoms/playerAtom.js
@@ -0,0 +1,21 @@
+import { atom } from "recoil";
+
+export const playState = atom({
+  key: "playState",
+  default: false,
+});
+
+export const playingTrackState = atom({
+  key: "playingTrackState",
+  default: "",
+});
+
+export const textState = atom({
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: 'login', // default value (aka initial value)
+});
+
+export const useremail = atom({
+  key: 'useremail', // unique ID (with respect to other atoms/selectors)
+  default: '', // default value (aka initial value)
+});
